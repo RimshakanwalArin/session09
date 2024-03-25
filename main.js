839b518ab25b796f5b79fa0ba04324f353a71340
RimshakanwalArin <160338555+RimshakanwalArin@users.noreply.github.com>
2024-03-25 14:13:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// logical oparetor
+var x = 10;
+var y = 20;
+console.log(x === 10 && y === 20); // true
+console.log(x <= 10 || y >= 22); // true
+console.log(x > 10); // false
+// console.log( true    &&  true    ); //true
+// console.log(   true   &&   false   ); //false
+// console.log(  false    &&   false   ); //false
+// console.log(   false   &&  true    );  //false
---
+++ main.ts
@@ -0,0 +1,12 @@
+// logical oparetor
+
+let x= 10;
+let y=  20;
+console.log(x===10 &&  y===20) // true
+console.log(x<=10 ||  y>=22)  // true
+console.log(x>10 !  )  // false
+
+// console.log( true    &&  true    ); //true
+// console.log(   true   &&   false   ); //false
+// console.log(  false    &&   false   ); //false
+// console.log(   false   &&  true    );  //false
